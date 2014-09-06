f28b537f3a89f94337f4e0456abdfff4d104d75c
App Academy Student <student@appacademy.io>
2014-09-05 17:06:10 -0700

    Separated tests into new file

--- HanoiGame/hanoiGame.js
+++ HanoiGame/hanoiGame.js
@@ -50,31 +50,4 @@ var run = function () {
   var game = new HanoiGame();  
 };
 
-// =============== Tests! 
-var testIsWon = function (game){ 
-  console.log("== isWon");
-  console.log("ideally, false, then true");
-  console.log(game.isWon());
-  game.stacks = [[], [3, 2, 1], [] ];
-  console.log(game.isWon());
-};
-
-var testIsValidMove = function (game) {
-  console.log("== validMove");
-  console.log("ideally: f, t, t, f, f");
-  
-  game.stacks = [[3], [2, 1], [] ];
-  console.log(game.isValidMove(0, 1));
-  console.log(game.isValidMove(1, 2));
-  console.log(game.isValidMove(1, 0));
-  console.log(game.isValidMove(2, 0));
-  console.log(game.isValidMove(8, 0));
-};
-
-var testAll = function (){
-  var game = new HanoiGame();
-  testIsWon(game);
-  testIsValidMove(game);
-};
-
-testAll();
+module.exports = HanoiGame
